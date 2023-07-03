82ea4da5682d5e0416a5ba1f23b06fddf545d6e3
cardenaz <william@cardenas.se>
2023-07-03 10:35:17 +0200

    input forms

--- airbnb-clone/app/components/modals/RegisterModal.tsx
+++ airbnb-clone/app/components/modals/RegisterModal.tsx
@@ -13,6 +13,8 @@ import {
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 const RegisterModal = () => {
 
@@ -45,6 +47,48 @@ const RegisterModal = () => {
      
     }
 
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+ 
+            <Heading title='Welcome to Airnbnb' subtitle='Create an account'/>
+
+            <Input 
+
+            id='email'
+            label='Email'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            
+            />
+                 <Input 
+
+id='name'
+label='Name'
+disabled={isLoading}
+register={register}
+errors={errors}
+required
+
+/>
+<Input 
+
+id='password'
+type='password'
+label='Password'
+disabled={isLoading}
+register={register}
+errors={errors}
+required
+
+/>
+
+
+            
+             </div>
+    )
+
 
     return (<Modal disabled={isLoading} 
         isOpen={registerModal.isOpen}
@@ -52,6 +96,7 @@ const RegisterModal = () => {
         actionLabel='Continue'
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
+        body={bodyContent}
     />); 
 }
 
