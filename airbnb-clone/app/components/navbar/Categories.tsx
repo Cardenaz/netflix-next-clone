06b452af9dc02a80d6670a312325a969061dbdb0
cardenaz <william@cardenas.se>
2023-07-05 17:10:11 +0200

    pä

--- airbnb-clone/app/components/navbar/Categories.tsx
+++ airbnb-clone/app/components/navbar/Categories.tsx
@@ -1,5 +1,7 @@
+'use client'; 
 import { categories } from "@/app/shared/domain/categories";
 import Container from "../Container"
+
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
 
