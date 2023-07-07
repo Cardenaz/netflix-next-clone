bf7f328418288250ea1714f77c18eaed376fffdf
cardenaz <william@cardenas.se>
2023-07-07 22:52:23 +0200

    tmr

---
+++ portfolio/app/components/Main.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head"
+
+
+const Main = () => {
+    return (
+        <div className="flex flex-col pt-10 pl-10 gap-8"> 
+    <div className="
+    "> 
+
+    <button className=
+    "h-12 px-20 font-semibold rounded-md bg-red-600 text-white items-center">
+        Hello, I'm a full stack developer based in Uppsala!
+    </button>
+    </div>
+<div > 
+    <h2 className="font-bold text-2xl">William Cardenas</h2>
+    <p>Digital Craftzman (Developer)</p></div>
+    </div>
+    
+    ) 
+}
+
+export default Main; 
---
+++ portfolio/app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+'use client'; 
+
+const Navbar = () => {
+    return <div> </div>
+}
+
+
+export default Navbar; 
