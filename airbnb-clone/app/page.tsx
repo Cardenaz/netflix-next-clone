90ca8abf79bb00d2f4c84c1e3eb8f8a946496f6c
cardenaz <william@cardenas.se>
2023-07-07 16:00:20 +0200

    yup

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,3 +1,4 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
@@ -6,6 +7,7 @@ import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings(); 
+  const currentUser = await getCurrentUser(); 
 
   if (listings.length === 0) {
     return (
@@ -20,26 +22,28 @@ export default async function Home() {
 
       <Container> 
         <div className="
-        pt-24 
-        grid 
-        grid-cols-1 
-        sm:grid-cols-2 
-        md:grid-cols-3 
-        lg:grid-cols-4
-        xl:grid-cols-5
-        2xl:grid-cols-6
-        gap-8
+       pt-24
+       grid 
+       grid-cols-1 
+       sm:grid-cols-2 
+       md:grid-cols-3 
+       lg:grid-cols-4
+       xl:grid-cols-5
+       2xl:grid-cols-6
+       gap-8
         
         
         "
         
         >
-          <div>{listings.map((listing:any) => {
+          {listings.map((listing:any) => {
             return (
               
               <ListingCard 
 
               key={listing.id}
+              data={listing}
+
             
               
               />
@@ -47,7 +51,7 @@ export default async function Home() {
        
             )
 
-          })}</div>
+          })}
 
  
         </div>
