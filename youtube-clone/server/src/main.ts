50b621b058ae48dfd8c99b2f5427bf53b9d8179f
cardenaz <william@cardenas.se>
2023-08-26 13:18:38 +0200

    nice

---
+++ youtube-clone/server/src/main.ts
@@ -0,0 +1,34 @@
+import express from 'express'; 
+import logger from './utils/logger';
+import { connectToDatabase, disconnectFromDatabase } from './utils/database';
+
+const PORT = process.env.PORT || 4000; 
+
+const app = express(); 
+
+const server = app.listen(PORT, async () => {
+    await connectToDatabase(); 
+    console.log(`Server listening at http://localhost:${PORT}`); 
+
+}); 
+
+const signals = ["SIGTERM", "SIGINT"]
+
+function gracefulShutdown(signal: string) {
+    process.on(signal, async () => {
+        logger.info("Goodbye, got signal", signal); 
+        server.close(); 
+
+        await disconnectFromDatabase(); 
+
+        logger.info(`My work here is done`); 
+
+        process.exit(0); 
+    }); 
+} 
+
+for (let i = 0; i < signals.length; i++){
+    gracefulShutdown(signals[i]); 
+}
+
+
